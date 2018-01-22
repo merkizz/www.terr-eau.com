7028357f507ae9971e018f1b2c13e5552eacd901
Thomas LAY <thomas.lay@rtlnet.fr>
2018-01-22 18:58:59 +0100

    Improve contact-section

--- v2/contact.php
+++ v2/contact.php
@@ -23,7 +23,7 @@
                 Toute notre équipe est à votre disposition pour tout renseignement.
             </p>
             <section class="contact-section">
-                <p class="text">
+                <div class="contact-section-text">
                     <strong>Terr'eau</strong>
                     <br>
                     <br>50, Avenue René Faugeras
@@ -32,8 +32,8 @@
                     <br>Tél. : 01 49 32 00 63
                     <br>Fax : 01 49 31 04 04
                     <br>Email : <a class="link" href="mailto:jardinier@terr-eau.com">jardinier@terr-eau.com</a>
-                </p>
-                <iframe class="embed" width="450" height="300" marginheight="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.386098991375!2d2.562767415674784!3d48.869915779288625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e611ec9d040719%3A0x41eb17e8cfe24ada!2s50+Avenue+Ren%C3%A9+Faugeras%2C+93220+Gagny!5e0!3m2!1sfr!2sfr!4v1516181826426" frameborder="0" marginwidth="0" scrolling="no"></iframe>
+                </div>
+                <iframe class="contact-section-map" width="450" height="300" marginheight="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.386098991375!2d2.562767415674784!3d48.869915779288625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e611ec9d040719%3A0x41eb17e8cfe24ada!2s50+Avenue+Ren%C3%A9+Faugeras%2C+93220+Gagny!5e0!3m2!1sfr!2sfr!4v1516181826426" frameborder="0" marginwidth="0" scrolling="no"></iframe>
             </section>
         </div>
 
