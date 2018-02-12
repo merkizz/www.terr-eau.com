05bb9a30ec33c6752f30866f29d526dd6b18b023
Thomas LAY <thomas.lay@rtlnet.fr>
2018-02-12 17:20:15 +0100

    Logos and footer

--- v2/widget/footer.php
+++ v2/widget/footer.php
@@ -1,12 +1,10 @@
 <div class="footer">
     <div class="slogan">Notre créativité à votre service</div>
     <div class="text">
-        SARL TERR'EAU
+        <img src="/v2/static/img/logo-440x105.png" height="50" alt="Logo SARL Terr'eau"/>
         <br>50, Avenue René Faugeras
         <br> 93220 GAGNY
-    </div>
-    <div class="text right">
-        Tél. : 01 49 32 00 63
+        <br>Tél. : 01 49 32 00 63
         <br>Fax : 01 49 31 04 04
         <br>Email : <a class="link" href="mailto:jardinier@terr-eau.com">jardinier@terr-eau.com</a>
     </div>
