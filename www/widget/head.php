3b7303f3f80c4ee9ba4c8b9c5151e52665cdb689
merkizz <thomas.piseth.lay@gmail.com>
2018-03-18 15:05:39 +0100

    Add favicon.ico

--- www/widget/head.php
+++ www/widget/head.php
@@ -8,6 +8,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
 <meta name="robots" content="index,follow" />
 
+<link rel="shortcut icon" type="image/x-icon" href="<?=$contextRoot?>/favicon.ico">
 <link type="text/plain" rel="author" href="<?=$contextRoot?>/humans.txt" />
 
 <link href="<?=$staticRoot?>/css/style.css" rel="stylesheet" type="text/css" />
